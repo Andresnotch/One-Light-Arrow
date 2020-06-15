edc5b04dc6c9ab0e49dd2612c497f7a000b01a50
@AndresHQuianty <andres.hidalgo@quianty.com>
2020-06-14 19:33:12 -0500

    +Add function to dissapear arrow when on contact with the player +When the player touches the arrow, it emits light itself +Added crosshair +Added player basic movement

---
+++ Scripts/Player.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+# Member variables
+const MOTION_SPEED = 100 # Pixels/second
+
+
+func _physics_process(_delta):
+	var motion = Vector2()
+	
+	if Input.is_action_pressed("w"):
+		motion += Vector2(0, -1)
+	if Input.is_action_pressed("s"):
+		motion += Vector2(0, 1)
+	if Input.is_action_pressed("a"):
+		motion += Vector2(-1, 0)
+	if Input.is_action_pressed("d"):
+		motion += Vector2(1, 0)
+	
+	motion = motion.normalized() * MOTION_SPEED
+
+	move_and_slide(motion)
+
+
+
+func _on_Area2D_area_entered(area):
+	$Light2D.visible = true
+	pass

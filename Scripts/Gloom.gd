7d3cab0a93d1c86d23205f75f15c278c0d698ef0
carlosquirarte27 <carlos.quirarte27@gmail.com>
2020-07-21 14:28:40 -0500

    * Menos danio para los glooms

--- Scripts/Gloom.gd
+++ Scripts/Gloom.gd
@@ -7,21 +7,22 @@ var istriggered = false
 func _process(delta):
 	var movement = Vector2()
 	# Set ray to player relative position
-	$LookDirection.cast_to = get_parent().get_node("Player").global_position - global_position
+	$LookDirection.cast_to = get_parent().get_parent().get_node("Player").global_position - global_position
 	var col = $LookDirection.get_collider()
 	if (col != null):
 		# If both are Kinematic
-		if (col.get_class() == get_parent().get_node("Player").get_class()):
+		if (col.get_class() == get_parent().get_parent().get_node("Player").get_class()):
 			# Set movement
-			movement = (get_parent().get_node("Player").global_position
+			movement = (get_parent().get_parent().get_node("Player").global_position
 			- global_position).normalized() * movement_speed
 	# Move when triggered
 	if(istriggered): move_and_slide(movement)
 	pass
 
 func _on_DamageArea_area_entered(_area):
-	life=0
-	queue_free()
+	if life > 0:
+		life -= 50
+	else: queue_free()
 	pass # Replace with function body.
 
 func _on_VisionArea_body_entered(_body):
